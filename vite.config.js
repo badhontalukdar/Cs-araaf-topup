209b4d95d0e5b1ec269189374dde6f5fbd98d631
badhontalukdar <talukdarbadhon284@gmail.com>
2025-04-14 15:02:22 +0600

    update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  base: "/Cs-araaf-topup/",
+  plugins: [react()],
 });
